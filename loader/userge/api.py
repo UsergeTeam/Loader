00a8c18f61b3d3f1c7548202cf6d841bc176da10
rking32 <sljohnwick@gmail.com>
2022-03-03 17:38:03 +0530

    better api docs

--- loader/userge/api.py
+++ loader/userge/api.py
@@ -86,7 +86,7 @@ async def get_repos() -> List[RepoInfo]:
     return await send_and_async_wait(GET_REPOS)
 
 
-async def add_repo(priority: int, branch: str, url: str) -> None:
+async def add_repo(priority: int, branch: str, url: str) -> bool:
     """
     add a plugins repo.
     Args:
@@ -95,15 +95,21 @@ async def add_repo(priority: int, branch: str, url: str) -> None:
             the plugins of this repo then use higher priority.
         branch: branch name of this repo.
         url: link to the repo
+
+    Returns:
+        True if success and else False.
     """
     return await send_and_async_wait(ADD_REPO, priority, branch, url)
 
 
-async def remove_repo(repo_id: int) -> None:
+async def remove_repo(repo_id: int) -> bool:
     """
     remove a plugins repo by its id.
     Args:
         repo_id: id from the RepoInfo object. hint: get_repos()
+
+    Returns:
+        True if success and else False.
     """
     return await send_and_async_wait(REMOVE_REPO, repo_id)
 
@@ -252,7 +258,7 @@ async def set_repo_priority(repo_id: int, priority: int) -> bool:
     return await edit_repo(repo_id, None, None, priority)
 
 
-async def add_constraints(c_type: str, data: List[str]) -> None:
+async def add_constraints(c_type: str, data: List[str]) -> bool:
     """
     add constraints to filter plugins or categories.
     Args:
@@ -262,25 +268,31 @@ async def add_constraints(c_type: str, data: List[str]) -> None:
             will be added to the project. if not found (`include`) then the loader will check
             for `exclude` constraints. if found then those plugins or categories will be ignored.
             if not found (`exclude`) then the loader will check for `in` constraints. if found
-            then the loader will add only plugin and categories in this constraint. other all
+            then the loader will add only plugins and categories in this constraint. other all
             things will be ignored.
         data: list of constraints. a constraint can be
             a plugin name (ping),
             a category name (admin/),
             a repo name followed by a plugin name (usergeteam.userge-plugins/ping) and
             a repo name followed by a category name (usergeteam.userge-plugins/admin/).
+
+    Returns:
+        True if success and else False.
     """
     return await send_and_async_wait(ADD_CONSTRAINTS, c_type, data)
 
 
-async def remove_constraints(c_type: Optional[str], data: List[str]) -> None:
+async def remove_constraints(c_type: Optional[str], data: List[str]) -> bool:
     """
     remove added constraints.
     Args:
         c_type: constraint type. hint: see docs of add_constraints().
             if None then all the given constraint data will be removed without considering
             the constraint type. if its there, then it will limit to the constraint type.
-        data: list of constraints. hint: see docs of add_constraints()
+        data: list of constraints. hint: see docs of add_constraints().
+
+    Returns:
+        True if success and else False.
     """
     return await send_and_async_wait(REMOVE_CONSTRAINTS, c_type, data)
 
@@ -294,13 +306,16 @@ async def get_constraints() -> List[Constraint]:
     return await send_and_async_wait(GET_CONSTRAINTS)
 
 
-async def clear_constraints(c_type: Optional[str]) -> None:
+async def clear_constraints(c_type: Optional[str]) -> bool:
     """
     clear all added constraints.
     Args:
         c_type: constraint type. hint: see docs of add_constraints(). if None then this will
             clear the all constraints. else, the constraint only related to this type
             will be cleared.
+
+    Returns:
+        True if success and else False.
     """
     return await send_and_async_wait(CLEAR_CONSTRAINTS, c_type)
 
